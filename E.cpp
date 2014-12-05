214bc21031e5e5d4b764f828db2753c1cb1d0a0e
olabknbit <al.byczynska@gmail.com>
2014-12-05 21:55:11 +0100

    Update E.cpp
    amended

--- E.cpp
+++ E.cpp
@@ -1,23 +1,25 @@
-#include <iostream>
+#include<iostream>
+
 using namespace std;
 
-int find(int *t,int liczba,int pocz,int kon){
-
-	if(pocz==kon-1){
-		if(t[pocz]==liczba) return pocz+1;
-		return -1;	
-	} 
-    int polowa=(pocz+kon)/2;
-   
-    if(liczba<=t[polowa]) {
-    	if(polowa-pocz==1){
-    		if(t[pocz]==liczba) return pocz+1;
-    		if(t[polowa]==liczba) return polowa+1;
-    		return -1;
-    	}
-    	return find(t, liczba, pocz, polowa+1);
-    }//tu powinno byæ polowa+1 ale wtedy nie dziala
-    else return find(t, liczba, polowa, kon);
+int index(int* t, int kon, int k)
+{
+    char x;
+    int pocz = 0;
+    int polowa = (pocz+kon)/2;
+    while(pocz != kon - 1)
+    {
+        if(t[polowa] >= k)
+            kon = polowa;
+        else
+            pocz = polowa;
+        polowa = (pocz+kon)/2;
+    }
+    if(t[pocz] == k)
+        return kon;
+    if(t[kon] == k)
+        return kon+1;
+    return -1;
 }
 
 int main(){
@@ -29,7 +31,6 @@ int main(){
     }
     for(int i=0; i< k; i++){//wczytujemy po kolei wszystkie liczby ktorych inddeksy mamy wskazac
         cin>>szukana;
-        cout<<find(t,szukana, 0, n)<<" ";
+        cout<<index(t,n,szukana)<<" ";
     }
 }
-
